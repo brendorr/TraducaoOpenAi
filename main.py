cd2a5f50b9459a7ce4697396cc85a033994a6039
marcio55afr <marcio55afr@gmail.com>
2023-03-11 18:46:22 -0300

    conexao openAI bem sucedida

---
+++ main.py
@@ -0,0 +1,15 @@
+import openai
+import regex
+import os
+import requests
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+r = openai.Completion.create( 
+  model="text-davinci-003",
+  prompt="Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
+
+print(r['choices'][0]["text"])
+
